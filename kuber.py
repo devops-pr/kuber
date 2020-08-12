2e30f57f40c498b27fac1d598eaaafc11b8d63e9
Piyush Raj <piyush.raj@outlook.in>
2020-08-12 13:40:25 +0530

    Added validation for GitHub

--- kuber.py
+++ kuber.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 from k8s_module import available_contexts
+from common import *
 
 
 class Kuber:
@@ -33,6 +34,8 @@ class Kuber:
         self.frame_content.pack()
 
 # LABEL
+
+        #Initial labels
         ttk.Label(self.frame_content, text="Git URL: ").grid(row = 4, column = 0, columnspan = 2, padx=7, sticky='sw')
         ttk.Label(self.frame_content, text="K8s Cluster: ").grid(row=12, column = 0, padx=7, sticky='sw')
         ttk.Label(self.frame_content, text="No. of pods: ").grid(row=12, column=1, padx=5, sticky='sw')
@@ -42,6 +45,13 @@ class Kuber:
         ttk.Label(self.frame_content, text="Enable Autoscaling: ").grid(row = 28, column = 1, padx=5, sticky='sw' )
         ttk.Label(self.frame_content, text="Maximum No of pods: ").grid(row = 36, column = 1, padx=7, sticky='w')
         ttk.Label(self.frame_content, text="Minimum No of pods: ").grid(row = 36, column = 0, padx=5, sticky='w')
+
+        #Error labels
+        self.invalid_url = ttk.Label(self.frame_content, text="Invalid URL!!! ", foreground = 'red')
+        self.inaccessible_url = ttk.Label(self.frame_content, text="Inaccessible URL!!! ", foreground = 'red')
+
+        #Success labels
+        self.accessible_url = ttk.Label(self.frame_content, text="\u2713", foreground = 'green')
 
 # ENTRY
         self.git_url_entry = ttk.Entry(self.frame_content, width=42, font = ('Arial', 10))
@@ -74,9 +84,10 @@ class Kuber:
 
 
 # BUTTONS
-        ttk.Button(self.frame_content, text='Validate').grid(row = 44, column = 0, padx=0, pady=5, sticky='e')
-        ttk.Button(self.frame_content, text='Clear').grid(row = 44, column = 1, padx=0, pady=5, sticky='w')
-
+        self.validate_button = ttk.Button(self.frame_content, text='Validate', command = self.validate)
+        self.validate_button.grid(row = 44, column = 0, padx=0, pady=5, sticky='e')
+        self.clear_button = ttk.Button(self.frame_content, text='Clear', command = self.clear)
+        self.clear_button.grid(row = 44, column = 1, padx=0, pady=5, sticky='w')
 
 # SPINBOXES
         self.min_no_of_pods = IntVar()
@@ -101,12 +112,38 @@ class Kuber:
         self.context_combobox.grid(row=16, column=0, padx=5, pady=5, sticky='nw')
         self.context_combobox.config(values=())
 
+        self.GIT_URL = self.git_url_entry.get()
+        self.DOCKER_HUB_USER = self.docker_hub_user_entry.get()
+        self.DOCKER_HUB_PASSWORD = self.docker_hub_password_entry.get()
+        self.CLUSTER_NAME = self.context.get()
+        self.NO_OF_PODS = self.no_of_pods.get()
+        self.APP_PORT = self.app_port_entry.get()
+        self.ENABLE_AUTOSCALING = self.enable_autoscaling.get()
+        self.MIN_POD = self.min_no_of_pods.get()
+        self.MAX_POD = self.max_no_of_pods.get()
+
     def enable_min_max(self):
         self.max_no_of_pods_spinbox.config(state = 'readonly')
         self.min_no_of_pods_spinbox.config(state = 'readonly')
     def disable_min_max(self):
         self.max_no_of_pods_spinbox.config(state='disabled')
         self.min_no_of_pods_spinbox.config(state='disabled')
+
+    def clear(self):
+        self.git_url_entry.delete(0, 'end')
+        self.docker_hub_user_entry.delete(0, 'end')
+        self.app_port_entry.delete(0, 'end')
+        self.docker_hub_password_entry.delete(0, 'end')
+
+    def validate(self):
+        if not validate_url(self.git_url_entry.get()):
+            self.invalid_url.grid(row=4, column=0, columnspan=2, padx=55, sticky='sw')
+        elif not verify_url_accessibility(self.git_url_entry.get()):
+            self.inaccessible_url.grid(row=4, column=0, columnspan=2, padx=55, sticky='sw')
+        else:
+            self.accessible_url.grid(row=4, column=0, columnspan=2, padx=55, sticky='sw')
+
+
 
 
 def main():
@@ -119,9 +156,6 @@ def main():
     current_context_name = current_context['name']
     feedback.context.set(current_context_name)
     feedback.context_combobox.config(values = all_contexts_tuple)
-
-    GIT_URL = feedback.git_url_entry.get()
-
     root.mainloop()
 
 
